2b919940db827f3253bcb0893a1f716e448565b3
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-06-23 13:16:34 +0800

    all

---
+++ index.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+        
+        <meta charset="utf-8" />
+        <title>4th PNHRS Week</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Health R and D in the New Normal : Moving towards universal health care" name="description" />
+        <meta content="kradyfatty" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- owl.carousel css -->
+        <link rel="stylesheet" href="assets/libs/owl.carousel/assets/owl.carousel.min.css">
+
+        <link rel="stylesheet" href="assets/libs/owl.carousel/assets/owl.theme.default.min.css">
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body data-bs-spy="scroll" data-bs-target="#topnav-menu" data-bs-offset="60">
+
+        <nav class="navbar navbar-expand-lg navigation fixed-top sticky">
+            <div class="container">
+                <a class="navbar-logo" href="index-2.html">
+                    <img src="assets/images/logo-dark.png" alt="" height="19" class="logo logo-dark">
+                    <img src="assets/images/logo-light.png" alt="" height="19" class="logo logo-light">
+                </a>
+
+                <button type="button" class="btn btn-sm px-3 font-size-16 d-lg-none header-item waves-effect waves-light" data-bs-toggle="collapse" data-bs-target="#topnav-menu-content">
+                    <i class="fa fa-fw fa-bars"></i>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="topnav-menu-content">
+                    <ul class="navbar-nav ms-auto" id="topnav-menu" >
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#news">News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#faqs">FAQs</a>
+                        </li>
+
+                    </ul>
+
+                    <div class="my-2 ms-lg-2">
+                        <a href="javascript: void(0);" class="btn btn-outline-success w-xs">Register</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <!-- hero section start -->
+        <section class="section hero-section bg-ico-hero" id="home">
+            <div class="bg-overlay bg-primary"></div>
+            <div class="container" style="margin-top: -100px;">
+                <div class="row align-items-center">
+                    <div class="col-lg-5">
+                        <div class="text-white-50">
+                            <div class="">
+                                <img src="assets/images/krad/logo.png" class="img-fluid" alt="Responsive image" style="max-width: 85%; height: auto;">
+                            </div>
+                            <!-- <p class="font-size-14">Health R and D in the New Normal : Moving towards Universal Health Care</p>
+                            <div class="mt-4">
+                                <img src="assets/images/krad/pnhrs.png" alt="" class="img-thumbnail" style="width: 30%; height: auto;">
+                                <img src="assets/images/krad/dost.png" alt="" class="rounded avatar-sm">
+                                <img src="assets/images/krad/pchrd.png" alt="" class="rounded avatar-sm">
+                                <img src="assets/images/krad/zchrd.png" alt="" class="rounded avatar-sm">
+                            </div> -->
+                            <!-- <p class="font-size-14">It will be as simple as occidental in fact to an English person, it will seem like simplified as a skeptical Cambridge</p>
+                            
+                            <div class="button-items mt-4">
+                                <a href="javascript: void(0);" class="btn btn-success">Get Whitepaper</a>
+                                <a href="javascript: void(0);" class="btn btn-light">How it work</a>
+                            </div> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="text-white-50">
+                            <h1 class="text-white fw-semibold mb-3">Health R and D in the New Normal :<br> Moving towards Universal Health Care</h1>
+                            <p class="font-size-14">It will be as simple as occidental in fact to an English person, it will seem like simplified as a skeptical Cambridge</p>
+                           
+                            <div class="col-md-6 float-right mt-4" style="float: right;">
+                                <img src="assets/images/krad/pnhrs.png" alt="" class="img-thumbnail" style="width: 40%; height: auto;">
+                                <img src="assets/images/krad/dost.png" alt="" class="rounded avatar-sm">
+                                <img src="assets/images/krad/pchrd.png" alt="" class="rounded avatar-sm">
+                                <img src="assets/images/krad/zchrd.png" alt="" class="rounded avatar-sm">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+     
+
+
+       
+       
+
+        <!-- Blog start -->
+        <section class="section bg-white" id="about">
+            <div class="container">
+
+                <div class="row" data-bs-toggle="modal" data-bs-target="#openmodal">
+                    <div class="col-xl-12">
+                        <div class="blog-box mb-4 mb-xl-0">
+                            <div class="mt-4 text-muted">
+                                <h5 class="text-primary mb-3"><i class="bx bx-news me-1"></i> SATELLITE SESSIONS</h5>
+                                  <div class="text-muted mt-4" style="cursor:pointer">
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Financing for National Health Security and Pandemic Preparedness</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Strengthening the National Health Research System’s Capacity for Response and Research</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Enhancing Regional Preparedness for Public Health Emergencies</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Strengthening the National Health Research System’s Capacity for Response and Research</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-12">
+                        <div class="blog-box mb-4 mb-xl-0">
+                            <div class="mt-4 text-muted">
+                                <h5 class="text-danger mb-3"><i class="bx bx-news me-1"></i> PRE-CONFERENCE SESSION</h5>
+                                <div class="text-muted mt-4" style="cursor:pointer">
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Oral Research Presentation Contest - Student</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Oral Research Presentation Contest - Professional</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Talk on Social Innovation in Health Research</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Moral Compass for Emergency Research and the Importance and Nature of Community Consultation</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Undergraduate Thesis Grant in Natural Products Competition</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>HERDIN</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Research Ethics Secretariat Training</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>3-Minute Pitch to Policymakers Competition</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-12">
+                        <div class="blog-box mb-4 mb-xl-0">
+                            <div class="mt-4 text-muted">
+                                <h5 class="text-success mb-3"><i class="bx bx-news me-1"></i> MAIN CONFERENCE SESSIONS</h5>
+                                <div class="text-muted mt-4" style="cursor:pointer">
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>14th PNHRS Week Celebration - Opening and Plenary 1</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Implications of the New Normal on Health Technology Assessment</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Enhancing the Country’s Bio-Preparedness for COVID-19 and other Emerging Infectious Diseases</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Harnessing International Collaborations and Engagement of Stakeholders to Maximize Health Research Impact</p>
+                                    <p class="mb-2"><i class="mdi mdi-circle-medium text-success me-1"></i>Plenary II and 14th PNHRS Week Celebration Closing</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <div class="modal fade" id="openmodal" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered  modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title mt-0" id="myExtraLargeModalLabel">Register Now!</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <i class="mdi mdi-alert-outline me-2"></i> Link for registration is not yet available.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="landing-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="mb-4">
+                            <img src="assets/images/logo-light.png" alt="" height="20">
+                        </div>
+    
+                        <p class="mb-2"><script>document.write(new Date().getFullYear())</script> © PNHRS. Design & Develop by DOST-IX</p>
+                    </div>
+                    <!-- <div class="col-lg-6">
+                        <div>
+                            <img src="assets/images/krad/pnhrs.png" alt="" class="img-thumbnail" style="width: 40%; height: auto;">
+                            <img src="assets/images/krad/dost.png" alt="" class="rounded avatar-sm">
+                            <img src="assets/images/krad/pchrd.png" alt="" class="rounded avatar-sm">
+                            <img src="assets/images/krad/zchrd.png" alt="" class="rounded avatar-sm">
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </footer>
+
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+        <script src="assets/libs/jquery.easing/jquery.easing.min.js"></script>
+        <script src="assets/libs/jquery-countdown/jquery.countdown.min.js"></script>
+        <script src="assets/libs/owl.carousel/owl.carousel.min.js"></script>
+        <script src="assets/js/pages/ico-landing.init.js"></script>
+        <script src="assets/js/app.js"></script>
+        <!-- <script>
+                $(function () {
+                    $('#test').on('click', function () {
+                       $("#openmodal").modal('hide');
+                    })
+                })
+            
+            </script> -->
+    </body>
+</html>
