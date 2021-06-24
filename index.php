a6500cc60bb1e1979ad712b216eb4bc0648fb554
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-06-24 10:01:56 +0800

    haha

--- index.php
+++ index.php
@@ -58,9 +58,9 @@
         <section class="section hero-section bg-ico-hero" id="home" style="height: 100vh;">
             <div class="bg-overlay bg-primary"></div>
             <div class="container" v-bind:style="[!isMobile() ? {'margin-top' : '-100px'} : {}]">
-                <div class="row ">
+                <div class="row" v-if="!isMobile()">
                     <div class="col-lg-5">
-                        <div class="text-white-50" v-if="!isMobile()">
+                        <div class="text-white-50">
                             <div class="">
                                 <img src="assets/images/krad/logo.png" class="img-fluid" alt="Responsive image" style="max-width: 85%; height: auto;">
                             </div>
@@ -68,6 +68,107 @@
                     </div>
                     <div class="col-lg-7">
                         <div class="text-white-50" v-if="type == 'main'">
+                            <h1 class="text-white fw-semibold mb-3"><span v-bind:style="[!isMobile() ? {'font-size' : '24px'} : {'font-size' : '15px'}]">Health R&D in the New Normal :</span><br><span v-bind:style="[!isMobile() ? {'font-size' : '32px'} : {'font-size' : '18px'}]"> Moving towards Universal Health Care</span></h1>
+                            <span class="font-size-14" style="margin-right: 20px;"><i class='bx bx-calendar'></i> August 09-13, 2021 </span>
+                            <span class="font-size-14"><i class='bx bx-map-pin'></i> Zamboanga City</span>
+                            <br class="mb-5">
+                            <div class="tab-content mt-4">
+                                <div class="tab-pane fade show active" id="v-pills-gen-ques" role="tabpanel">
+                                    <div>
+                                        <div id="gen-ques-accordion" class="accordion custom-accordion">
+                                            <div class="mb-3">
+                                                <a style="cursor: pointer;" @click="openhaha(1)" class="accordion-list">
+                                                    <div>Sattelite Sessions</div>
+                                                    <i class="bx bx-plus accor-plus-icon"></i>
+                                                </a>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <a style="cursor: pointer;" @click="openhaha(2)" class="accordion-list">
+                                                    <div>Main Conference Sessions</div>
+                                                    <i class="bx bx-plus accor-plus-icon"></i>
+                                                </a>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <a style="cursor: pointer;" @click="openhaha(3)" class="accordion-list">
+                                                    <div>Pre-Conference Sessions</div>
+                                                    <i class="bx bx-plus accor-plus-icon"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-white-50" v-else-if="type == 1">
+                            <div class="avatar-sm mx-auto mb-4 float-end" @click="openhaha('main')" style="cursor:pointer;">
+                                <span class="avatar-title rounded-circle bg-light font-size-24">
+                                    <i class="bx bx-x-circle text-danger"></i>
+                                </span>
+                            </div>
+                            <h1 class="text-white fw-semibold mb-4">Sattelite Sessions :</h1>
+                            <hr>
+                            <div class="text-white mt-4" style="cursor:pointer">
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_B1HGEtFjQjyqjJQXl_KWGw','August 03, 2021')" class="mb-2 text-truncate" ><i class="mdi mdi-circle-medium text-warning me-1"></i>Financing for National Health Security and Pandemic Preparedness</p>
+                                <p @click="openmodal('','August 04, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Strengthening the National Health Research System’s Capacity for Response and Research</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_BUZ-ntn_SI-I9qMJyMVuKA','August 05, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Enhancing Regional Preparedness for Public Health Emergencies</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_6k1-qA-5RUe7F231-IPuow','August 06, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Ensuring Good Governance and Accountability in the New Normal</p>
+                            </div>
+                        </div>
+                        <div class="text-white-50" v-else-if="type == 2"> 
+                            <div class="avatar-sm mx-auto mb-4 float-end" @click="openhaha('main')" style="cursor:pointer;">
+                                <span class="avatar-title rounded-circle bg-light font-size-24">
+                                    <i class="bx bx-x-circle text-danger"></i>
+                                </span>
+                            </div>
+                            <h1 class="text-white fw-semibold mb-4">Pre-Conference Sessions :</h1>
+                            <hr>
+                            <div class="text-white mt-4" style="cursor:pointer">
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_HSzgJC2YSMKtazgiE_MFiA','August 09, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Oral Research Presentation Contest - Student</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_wvTDUmJWQV23CQEbFnm3DQ','August 09, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Oral Research Presentation Contest - Professional</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_4V-Qlp41TyCDokWC1bot3A','August 09, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Talk on Social Innovation in Health Research</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_s-isBFZESkinjrB_YXkQ8g','August 10, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Moral Compass for Emergency Research and the Importance and Nature of Community Consultation</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_aut-ZSkBRAOMB6cFyigSjg','August 10, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Undergraduate Thesis Grant in Natural Products Competition</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_E0AnW6IsT6mNE-O7tZmxTw','August 11, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>HERDIN</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_ROZhY35mR36O0AOiHoe1yQ','August 11, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Research Ethics Secretariat Training</p>
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_KvBNGA_GT3q9TGLaxFEjIg','August 11, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>3-Minute Pitch to Policymakers Competition</p>
+                            </div>
+                        </div>
+                        <div class="text-white-50" v-else>
+                            <div class="avatar-sm mx-auto mb-4 float-end" @click="openhaha('main')" style="cursor:pointer;">
+                                <span class="avatar-title rounded-circle bg-light font-size-24">
+                                    <i class="bx bx-x-circle text-danger"></i>
+                                </span>
+                            </div>
+                             <h1 class="text-white fw-semibold mb-4">Main Conference Sessions :</h1>
+                             <hr>
+                             <div class="text-white mt-4" style="cursor:pointer">
+                                <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_5aRz2YXGQBa_bxW6RRTdJg', 'August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>14th PNHRS Week Celebration - Opening and Plenary 1</p>
+                                <p @click="openmodal('','n/a')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Implications of the New Normal on Health Technology Assessment</p>
+                                <p @click="openmodal('','n/a')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Enhancing the Country’s Bio-Preparedness for COVID-19 and other Emerging Infectious Diseases</p>
+                                <p @click="openmodal('','n/a')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Harnessing International Collaborations and Engagement of Stakeholders to Maximize Health Research Impact</p>
+                                <p @click="openmodal('','n/a')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Plenary II and 14th PNHRS Week Celebration Closing</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" v-else>
+                    <div class="col-lg-12">
+                        <div class="media">
+                                <div class="media-body float-end">
+                                <div class="text-white">
+                                    <p style="margin-bottom: -1px;">Health R&D in the New Normal :</p>
+                                    <h6 class="text-white mb-1">Moving towards Universal Health Care</h6>
+                                </div>
+                            </div>
+                            <div>
+                                <img src="assets/images/krad/logo.png" alt="" class="avatar-md rounded-circle img-thumbnail">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                    <div class="text-white-50" v-if="type == 'main'">
                             <h1 class="text-white fw-semibold mb-3"><span v-bind:style="[!isMobile() ? {'font-size' : '24px'} : {'font-size' : '15px'}]">Health R&D in the New Normal :</span><br><span v-bind:style="[!isMobile() ? {'font-size' : '32px'} : {'font-size' : '18px'}]"> Moving towards Universal Health Care</span></h1>
                             <span class="font-size-14" style="margin-right: 20px;"><i class='bx bx-calendar'></i> August 09-13, 2021 </span>
                             <span class="font-size-14"><i class='bx bx-map-pin'></i> Zamboanga City</span>
