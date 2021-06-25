32aae4c1225fe2c94f8b55eca33c14cb36d56f7c
arismoratalla <arismoratalla@gmail.com>
2021-06-25 16:16:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -163,11 +163,13 @@
                                     <h1 class="text-white fw-semibold mb-4">Main Conference Sessions :</h1>
                                     <hr>
                                     <div class="text-white mt-4" style="cursor:pointer">
-                                        <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_5aRz2YXGQBa_bxW6RRTdJg', 'August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>14th PNHRS Week Celebration - Opening and Plenary 1</p>
-                                        <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_-1fQ80W0RkiHE00v35EuEg','August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Implications of the New Normal on Health Technology Assessment</p>
-                                        <p @click="openmodal('https://us02web.zoom.us/meeting/register/tZwtce6tqjMvHtdrE5V-Lp8zxdK007HBwqXn','August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Enhancing the Country’s Bio-Preparedness for COVID-19 and other Emerging Infectious Diseases</p>
-                                        <p @click="openmodal('https://us02web.zoom.us/meeting/register/tZMpdOqupz0sE93vzwng7HAXBcziWyH68Fhb','August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Harnessing International Collaborations and Engagement of Stakeholders to Maximize Health Research Impact</p>
-                                        <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_s8n4xox2Sq25zNmwON-uTw','August 13, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Plenary II and 14th PNHRS Week Celebration Closing</p>
+                                        <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_5aRz2YXGQBa_bxW6RRTdJg', 'August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>14th PNHRS Week Celebration - Opening</p>
+                                        <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_5aRz2YXGQBa_bxW6RRTdJg', 'August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Plenary 1: Changing Landscape of Universal Health Care: Paradigm Shifts in Health R&D</p>
+                                        <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_-1fQ80W0RkiHE00v35EuEg','August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Parallel Session 1 : Implications of the New Normal on Health Technology Assessment</p>
+                                        <p @click="openmodal('https://us02web.zoom.us/meeting/register/tZwtce6tqjMvHtdrE5V-Lp8zxdK007HBwqXn','August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Parallel Session 2 : Enhancing the Country’s Bio-Preparedness for COVID-19 and other Emerging Infectious Diseases</p>
+                                        <p @click="openmodal('https://us02web.zoom.us/meeting/register/tZMpdOqupz0sE93vzwng7HAXBcziWyH68Fhb','August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Parallel Session 3 : Harnessing International Collaborations and Engagement of Stakeholders to Maximize Health Research Impact</p>
+                                        <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_s8n4xox2Sq25zNmwON-uTw','August 13, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Plenary 2: LGU COVID-19 Responses: Sharing of Collaborative Practices in Disease Prevention and Control</p>
+                                        <p @click="openmodal('https://us02web.zoom.us/webinar/register/WN_s8n4xox2Sq25zNmwON-uTw','August 13, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Closing and Awarding Ceremonies</p>
                                     </div>
                                 </div>
                             </div>
@@ -262,7 +264,7 @@
                     <div class="col-xl-12">
                         <div class="blog-box mb-4 mb-xl-0">
                             <div class="mt-4 text-muted">
-                                <h5 class="text-primary mb-3"><i class="bx bx-news me-1"></i> SATELLITE SESSIONS</h5>
+                                <h5 class="text-primary mb-3"><i class="bx bx-news me-1"></i> SATELLITE SESSIONS (03 -06 August 2021)</h5>
                                 <div class="text-muted mt-4" style="cursor:pointer">
                                     <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_B1HGEtFjQjyqjJQXl_KWGw','August 03, 2021')" class="mb-2 text-truncate" ><i class="mdi mdi-circle-medium text-warning me-1"></i>Financing for National Health Security and Pandemic Preparedness &nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-calendar'></i> August 03, 2021 | 8 AM - 12 NN</span>&nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-map-pin'></i> South Luzon Cluster</span></p>
                                     <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_0wrnbsM1R5iGsWKqRt8oCQ','August 04, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Strengthening the National Health Research System’s Capacity for Response and Research &nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-calendar'></i> August 04, 2021 | 8 AM - 12 NN</span>&nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-map-pin'></i> North Luzon Cluster</span></p>
@@ -276,10 +278,10 @@
                     <div class="col-xl-12">
                         <div class="blog-box mb-4 mb-xl-0">
                             <div class="mt-4 text-muted">
-                                <h5 class="text-danger mb-3"><i class="bx bx-news me-1"></i> PRE-CONFERENCE SESSION</h5>
+                                <h5 class="text-danger mb-3"><i class="bx bx-news me-1"></i> PRE-CONFERENCE SESSION (09 -11 August 2021)</h5>
                                 <div class="text-muted mt-4" style="cursor:pointer">
                                     <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_HSzgJC2YSMKtazgiE_MFiA','August 09, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Oral Research Presentation - Student Category&nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-calendar'></i> August 09, 2021 | 8 AM - 12 NN</span></p>
-                                    <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_wvTDUmJWQV23CQEbFnm3DQ','August 09, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Oral Research Presentation - Professional Category&nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-calendar'></i> August 03, 2021 | 1 - 4 PM</span></p>
+                                    <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_wvTDUmJWQV23CQEbFnm3DQ','August 09, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Oral Research Presentation - Professional Category&nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-calendar'></i> August 09, 2021 | 1 - 4 PM</span></p>
                                     <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_4V-Qlp41TyCDokWC1bot3A','August 09, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Talk on Social Innovation in Health Research&nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-calendar'></i> August 09, 2021 | 8 AM - 12 NN</span></p>
                                     <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_s-isBFZESkinjrB_YXkQ8g','August 10, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Moral Compass for Emergency Research and the Importance and Nature of Community Consultation&nbsp;&nbsp;&nbsp;<span class="font-size-10"><i class='bx bx-calendar'></i> August 10, 2021 | 8 AM - 12 NN</span></p>
                                     <!--p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_aut-ZSkBRAOMB6cFyigSjg','August 10, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Undergraduate Thesis Grant in Natural Products Competition</p-->
@@ -295,7 +297,7 @@
                     <div class="col-xl-12">
                         <div class="blog-box mb-4 mb-xl-0">
                             <div class="mt-4 text-muted">
-                                <h5 class="text-warning mb-3"><i class="bx bx-news me-1"></i> MAIN CONFERENCE SESSIONS</h5>
+                                <h5 class="text-warning mb-3"><i class="bx bx-news me-1"></i> MAIN CONFERENCE SESSIONS (12 -13 August 2021)</h5>
                                 <div class="text-muted mt-4" style="cursor:pointer">
                                     <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_5aRz2YXGQBa_bxW6RRTdJg', 'August 12, 2021')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>14th PNHRS Week Celebration - Opening and Plenary 1</p>
                                     <p @click="openmodal1('https://us02web.zoom.us/webinar/register/WN_B1HGEtFjQjyqjJQXl_KWGw','n/a')" class="mb-2 text-truncate"><i class="mdi mdi-circle-medium text-warning me-1"></i>Implications of the New Normal on Health Technology Assessment</p>
