a11dc94ce09cf220d2a9bfec2efdf6b2df796b89
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-07-29 16:18:40 +0800

    Wew

--- index.php
+++ index.php
@@ -121,7 +121,7 @@
                         <img class="faqs" src="assets/images/landing/new/designs/faqs.png" >
                         <a @click="view('faqs')"><img class="faq" src="assets/images/landing/new/designs/faq.png" onmouseover="this.src='assets/images/landing/new/designs/e-5.png'" onmouseout="this.src='assets/images/landing/new/designs/faq.png'"></a>
                         <img class="copy" src="assets/images/landing/new/designs/copyright.png" >
-                        <!-- <img class="plane" src="assets/images/landing/new/designs/plane.png"> -->
+                        <img class="plane" src="assets/images/landing/new/designs/plane.png">
                      
                         
                         
